069c07fc083f280a5b2f76e1c2ffc64ffabda7fe
Brandon Chen <parkghost@gmail.com>
2013-04-27 02:32:13 +0800

    use html markup

--- web/404.go
+++ web/404.go
@@ -7,7 +7,17 @@ import (
 
 func init() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, `
+		fmt.Fprintf(w, page404)
+	})
+}
+
+var page404 = `
+<html>
+<head>
+	<title>404 file not found</title>
+</head>
+<body>
+	<pre>
         _    _     ___     _    _        _       _
        |:|  |:|   /:::\   |:|  |:|      |_ | |  |_
        |:|  |:|  /:/'\:\  |:|  |:|      |  | |_ |_
@@ -18,6 +28,8 @@ func init() {
             |:| |:\   |:|      |:|  |_ / \ | | |\ | | \
             |:|  \:\_/:/       |:|  |  \_/ \_/ | \| |_/
             |:|   \:::/        |:|
-            '"'    '"'         '"'`)
-	})
-}
+            '"'    '"'         '"'
+	</pre>
+</body>
+</html>
+`
